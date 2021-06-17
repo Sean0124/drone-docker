75ab957120cd3c4e9ec220bb68b524ae0f2e3b6b
sean <sean.x.dbm@gmail.com>
2021-06-17 16:40:00 +0800

    ci

--- mysql/mysql_test.go
+++ mysql/mysql_test.go
@@ -1,20 +1,14 @@
 package mysql
 
-import (
-	docker "drone/drone-docker"
-	"github.com/sirupsen/logrus"
-	"testing"
-)
-
-func TestTagInset(t *testing.T) {
-	tagStoreMysql,err := docker.InitTagStore("mysql",
-		docker.WithUrl("root:5ziEppim@tcp(mysql-2580-0.tripanels.com:2580)/tags?charset=utf8"),
-	)
-	if err != nil {
-		logrus.Fatal(err)
-		//panic("init registry failed")
-	}
-
-	tagStoreMysql.TagInset()
-
-}
+//func TestTagInset(t *testing.T) {
+//	tagStoreMysql,err := docker.InitTagStore("mysql",
+//		docker.WithUrl("root:5ziEppim@tcp(mysql-2580-0.tripanels.com:2580)/tags?charset=utf8"),
+//	)
+//	if err != nil {
+//		logrus.Fatal(err)
+//		//panic("init registry failed")
+//	}
+//
+//	tagStoreMysql.TagInset()
+//
+//}
